c74fdb3a9a7963abdef1de7cb492a2fc7818bd1a
github username <github email address>
2022-07-24 17:06:28 +0100

    minute 16:30

--- tetris.js
+++ tetris.js
@@ -60,5 +60,85 @@ const shapes = [
   new Tetris(0, 0, [
     [1, 1],
     [1, 1]
+  ]),
+  new Tetris(0, 48, [
+    [0, 0, 0],
+    [1, 1, 0],
+    [0, 1, 1]
   ])
 ]
+
+let gameMap
+let gameOver
+let currentShape
+let nextShape
+let score
+let initialTwoDArr
+let whiteLineThickness = 4
+
+let gameLoop = () => {
+  setInterval(update, 1000 / gameSpeed)
+  setInterval(draw, 1000 / framePerSecond)
+}
+
+let update = () => {}
+
+let drawRect = (x, y, width, height, color) => {
+  ctx.fillStyle = color
+  ctx.fillRect(x, y, width, height)
+}
+
+let drawBackground = () => {
+  drawRect(0, 0, canvas.width, canvas.height, '#bca0dc')
+  for (let i = 0; i < squareCountX + 1; i++) {
+    drawRect(
+      size * i - whiteLineThickness,
+      0,
+      whiteLineThickness,
+      canvas.height,
+      'white'
+    )
+  }
+  for (let i = 0; i < squareCountY + 1; i++) {
+    drawRect(
+      0,
+      size * i - whiteLineThickness,
+      canvas.width,
+      whiteLineThickness,
+
+      'white'
+    )
+  }
+}
+
+let draw = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  drawBackground()
+  drawSquares()
+  drawCurrentTetris()
+  if (gameOver) {
+    drawGameOver()
+  }
+}
+
+let getRandomShape = () => {
+  return Object.create(shapes[Math.floor(Math.random() * shapes.length)])
+}
+
+let resetVars = () => {
+  initialTwoDArr = []
+  for (let i = 0; i < squareCountY; i++) {
+    let temp = []
+    for (let j = 0; j < squareCountX; j++) {
+      temp.push({ imageX: -1, imageY: -1 })
+    }
+    initialTwoDArr.push(temp)
+  }
+  score = 0
+  gameOver = false
+  currentShape = getRandomShape()
+  nextShape = getRandomShape()
+  gameMap = initialTwoDArr
+}
+
+gameLoop()
