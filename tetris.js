84b0abd10887ab04acfb249572f59176a7bde3ed
github username <github email address>
2022-07-24 16:04:53 +0100

    create tetris block arrays

--- tetris.js
+++ tetris.js
@@ -26,3 +26,39 @@ const framePerSecond = 24
 const gameSpeed = 5
 const canvas = document.getElementById('canvas')
 const image = document.getElementById('image')
+const ctx = canvas.getContext('2d')
+const squareCountX = canvas.width / size
+const squareCountY = canvas.height / size
+
+const shapes = [
+  new Tetris(0, 120, [
+    [0, 1, 0],
+    [0, 1, 0],
+    [1, 1, 0]
+  ]),
+  new Tetris(0, 96, [
+    [0, 0, 0],
+    [1, 1, 1],
+    [0, 1, 0]
+  ]),
+  new Tetris(0, 72, [
+    [0, 1, 0],
+    [0, 1, 0],
+    [0, 1, 1]
+  ]),
+  new Tetris(0, 48, [
+    [0, 0, 0],
+    [0, 1, 1],
+    [1, 1, 0]
+  ]),
+  new Tetris(0, 24, [
+    [0, 0, 1, 0],
+    [0, 0, 1, 0],
+    [0, 0, 1, 0],
+    [0, 0, 1, 0]
+  ]),
+  new Tetris(0, 0, [
+    [1, 1],
+    [1, 1]
+  ])
+]
